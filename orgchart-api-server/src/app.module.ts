1fdcf25596d5a106274d9c0397eef36877d0e345
Binyam Berhanu <98868076+binyam455@users.noreply.github.com>
2025-12-04 18:52:38 +0300

    Modified app.module.ts file

--- orgchart-api-server/src/app.module.ts
+++ orgchart-api-server/src/app.module.ts
@@ -1,24 +1,31 @@
 import { Module } from '@nestjs/common';
 import { OrgChart } from './entities/orgchart.entity'
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrgChartController } from './controllers/orgchart.controller';
-import { DataService } from './app.service';
+import { OrgChartController } from './modules/orgchart/controllers/orgchart.controller';
+import { OrgChartService } from './modules/orgchart/services/orgchart.service';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
+      ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'abc123',
-      database: 'orga_structure',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [OrgChart],
-      synchronize: true,
+      synchronize: false,
+      migrationsRun: false,
     }),
     TypeOrmModule.forFeature([OrgChart]),
   ],
-  controllers: [OrgChartController],
-  providers: [DataService]
+  controllers: [OrgChartController, AppController],
+  providers: [OrgChartService, AppService]
 })
 export class AppModule {}
